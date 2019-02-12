d9aec13047aa841596523da2566b8939fc7b68ce
simonrowlands <si_rhys96@hotmail.co.uk>
2019-02-12 11:11:21 +0000

    Initial Project Commit

---
+++ NY Times Most Popular/NY Times Most Popular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NY Times Most Popular
+//
+//  Created by Simon Rowlands on 12/02/2019.
+//  Copyright © 2019 simonrowlands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NY Times Most Popular/NY Times Most PopularTests/NY_Times_Most_PopularTests.swift
@@ -0,0 +1,34 @@
+//
+//  NY_Times_Most_PopularTests.swift
+//  NY Times Most PopularTests
+//
+//  Created by Simon Rowlands on 12/02/2019.
+//  Copyright © 2019 simonrowlands. All rights reserved.
+//
+
+import XCTest
+@testable import NY_Times_Most_Popular
+
+class NY_Times_Most_PopularTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
